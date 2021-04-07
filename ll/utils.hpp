ad3744f1a0b1147f9ca4d3ae8d2d5decc53886d4
Layle <luca@marcelli.ch>
2021-04-07 22:44:50 +0200

    ayy

--- ll/utils.hpp
+++ ll/utils.hpp
@@ -7,25 +7,25 @@
 
 namespace utils {
     template<typename T>
-	std::vector<size_t> find_pattern(const std::vector<T>& data, std::vector<ll::pattern_t> pattern) {
-		std::vector<size_t> indices;
+    std::vector<size_t> find_pattern(const std::vector<T>& data, std::vector<ll::pattern_t> pattern) {
+        std::vector<size_t> indices;
 
-		for (size_t i = 0; i < data.size(); ++i) {
-			bool found = false;
+        for (size_t i = 0; i < data.size(); ++i) {
+            bool found = false;
 
-			for (size_t j = 0; j < pattern.size(); ++j) {
-				found = (pattern[j].has_value() && i + j < data.size() && data[i + j] == pattern[j]) || !pattern[j].has_value();
+            for (size_t j = 0; j < pattern.size(); ++j) {
+                found = (pattern[j].has_value() && i + j < data.size() && data[i + j] == pattern[j]) || !pattern[j].has_value();
 
-				if (!found) break;
-			}
+                if (!found) break;
+            }
 
-			if (found) indices.push_back(i);
-		}
+            if (found) indices.push_back(i);
+        }
 
-		return indices;
-	}
+        return indices;
+    }
 
-	template<typename T = uint8_t>
+    template<typename T = uint8_t>
     std::vector<T> read_file(const std::filesystem::path& path) {
         std::ifstream file(path, std::ios::binary);
 
@@ -39,8 +39,8 @@ namespace utils {
         return buffer;
     }
 
-	template<typename T = uint8_t>
-	std::vector<T> slice(const std::vector<T>& data, size_t start, size_t end) {
-		return std::vector<T>(data.begin() + start, data.begin() + end);
-	}
+    template<typename T = uint8_t>
+    std::vector<T> slice(const std::vector<T>& data, size_t start, size_t end) {
+        return std::vector<T>(data.begin() + start, data.begin() + end);
+    }
 }
